b554b029297a0afd61bd17f7eac6af1171e306c8
Woongbin06 <kob05314@gmail.com>
2022-09-17 18:00:53 +0900

    commit

--- src/main/java/com/board/woongbin/board/domain/Board.java
+++ src/main/java/com/board/woongbin/board/domain/Board.java
@@ -8,8 +8,8 @@ import lombok.NoArgsConstructor;
 import javax.persistence.*;
 
 @Getter
-@Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
 public class Board {
 
     @Id
--- src/test/java/com/board/woongbin/board/BoardRepositoryTest.java
+++ src/test/java/com/board/woongbin/board/BoardRepositoryTest.java
@@ -4,25 +4,23 @@ import com.board.woongbin.board.domain.Board;
 import com.board.woongbin.board.repository.BoardRepository;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@SpringBootTest
+@ExtendWith(SpringExtension.class)
+@DataJpaTest
 public class BoardRepositoryTest {
     String title;
     String content;
 
-    @Autowired
-    BoardRepository boardRepository;
-
-    @AfterEach
-    public void clean() {
-        boardRepository.deleteAll();
-    }
+    @Autowired BoardRepository boardRepository;
 
     @Test
     public void save() {
@@ -34,14 +32,24 @@ public class BoardRepositoryTest {
                 .content(content)
                 .writer("강웅빈")
                 .build());
+
+        Board result = boardRepository.findById(1L).get();
+        assertThat(result.getWriter()).isEqualTo("강웅빈");
+        assertThat(result.getTitle()).isEqualTo(title);
+        assertThat(result.getContent()).isEqualTo(content);
     }
 
     @Test
     public void findAll() {
-        List<Board> list = boardRepository.findAll();
-        Board board = list.get(0);
+        long boardsCnt = boardRepository.count();
+        List<Board> boards = boardRepository.findAll();
 
-        assertThat(board.getTitle()).isEqualTo(title);
-        assertThat(board.getContent()).isEqualTo(content);
+        assertThat(boardsCnt).isEqualTo(boards.size());
+    }
+
+    @Test
+    public void delete() {
+        Board entity = boardRepository.findById(1L).get();
+        boardRepository.delete(entity);
     }
 }
